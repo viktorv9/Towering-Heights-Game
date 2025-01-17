b7c20485677bf2328abc80ba4ac1300f5df7c1a0
viktorv9 <38245893+viktorv9@users.noreply.github.com>
2025-01-17 14:42:37 +0100

    pickup system

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    [SerializeField] private float pickupCollisionDuration;
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] private Material collidingMaterial;
+
+    private MeshRenderer meshRenderer;
+    
+    private List<GameObject> collidersInTrigger = new ();
+    private float timeCollisionStart = 0;
+
+    private void Start() {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update() {
+        if (timeCollisionStart != 0) {
+            if (Time.time - timeCollisionStart > pickupCollisionDuration) {
+                Debug.Log("Picked up object! New Pickup!!!");
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!other.gameObject.CompareTag("Block")) return;
+        collidersInTrigger.Add(other.gameObject);
+        
+        if (collidersInTrigger.Count > 0) {
+            if (timeCollisionStart == 0) timeCollisionStart = Time.time;
+            meshRenderer.material = collidingMaterial;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other) {
+        if (!other.gameObject.CompareTag("Block")) return;
+        collidersInTrigger.Remove(other.gameObject);
+        
+        if (collidersInTrigger.Count == 0) {
+            timeCollisionStart = 0;
+            meshRenderer.material = defaultMaterial;
+        }
+    }
+}
