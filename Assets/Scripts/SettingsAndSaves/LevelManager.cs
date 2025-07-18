7d28d03223e4630b6494d1ea963c00aa517cf91a
viktorv9 <38245893+viktorv9@users.noreply.github.com>
2025-07-18 18:27:40 +0200

    remove outdated code

--- Assets/Scripts/SettingsAndSaves/LevelManager.cs
+++ Assets/Scripts/SettingsAndSaves/LevelManager.cs
@@ -11,54 +11,54 @@ public class LevelManager : MonoBehaviour {
     
     // TODO: This script saves data to JSON, but the SaveSystem/SaveData classes write to binary.
     // TODO: Convert or delete this code and work with SaveSystem!
-
-    [SerializeField] private List<Button> uiButtons;
-    
-    public class PlayerSaveData {
-        public int playerLevel;
-        public int unlockedLevels;
-    }
-
-    public static int playerLevel;
-    public static int unlockedLevels;
-
-    private void Start() {
-        LoadGame();
-        
-        int i = 0;
-        foreach (Button uiButton in uiButtons) {
-            uiButton.interactable = i <= unlockedLevels;
-            i++;
-        }
-        
-        playerLevel++;
-        unlockedLevels++;
-        SaveGame();
-    }
-
-    private static void SaveGame() {
-        PlayerSaveData playerData = new PlayerSaveData();
-        playerData.playerLevel = playerLevel;
-        playerData.unlockedLevels = unlockedLevels;
-
-        string json = JsonUtility.ToJson(playerData);
-        string path = Application.persistentDataPath + "/PlayerSaveData.json";
-        File.WriteAllText(path, json);
-    }
-
-    private static void LoadGame() {
-        string path = Application.persistentDataPath + "/PlayerSaveData.json";
-        if (File.Exists(path)) {
-            string json = File.ReadAllText(path);
-            PlayerSaveData loadedData = JsonUtility.FromJson<PlayerSaveData>(json);
-
-            playerLevel = loadedData.playerLevel;
-            unlockedLevels = loadedData.unlockedLevels;
-        } else {
-            playerLevel = 0;
-            unlockedLevels = 0;
-        }
-    }
+    //
+    // [SerializeField] private List<Button> uiButtons;
+    //
+    // public class PlayerSaveData {
+    //     public int playerLevel;
+    //     public int unlockedLevels;
+    // }
+    //
+    // public static int playerLevel;
+    // public static int unlockedLevels;
+    //
+    // private void Start() {
+    //     LoadGame();
+    //     
+    //     int i = 0;
+    //     foreach (Button uiButton in uiButtons) {
+    //         uiButton.interactable = i <= unlockedLevels;
+    //         i++;
+    //     }
+    //     
+    //     playerLevel++;
+    //     unlockedLevels++;
+    //     SaveGame();
+    // }
+    //
+    // private static void SaveGame() {
+    //     PlayerSaveData playerData = new PlayerSaveData();
+    //     playerData.playerLevel = playerLevel;
+    //     playerData.unlockedLevels = unlockedLevels;
+    //
+    //     string json = JsonUtility.ToJson(playerData);
+    //     string path = Application.persistentDataPath + "/PlayerSaveData.json";
+    //     File.WriteAllText(path, json);
+    // }
+    //
+    // private static void LoadGame() {
+    //     string path = Application.persistentDataPath + "/PlayerSaveData.json";
+    //     if (File.Exists(path)) {
+    //         string json = File.ReadAllText(path);
+    //         PlayerSaveData loadedData = JsonUtility.FromJson<PlayerSaveData>(json);
+    //
+    //         playerLevel = loadedData.playerLevel;
+    //         unlockedLevels = loadedData.unlockedLevels;
+    //     } else {
+    //         playerLevel = 0;
+    //         unlockedLevels = 0;
+    //     }
+    // }
 
     public void LoadLevel(string sceneName) {
         SceneManager.LoadScene(sceneName);
