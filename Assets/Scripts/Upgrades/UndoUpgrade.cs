ba13ad9732b819849e2c6afa6b422591728200d4
viktorv9 <38245893+viktorv9@users.noreply.github.com>
2025-08-15 09:49:17 +0200

    undo upgrade: add ui and prevent cooldown skipping

--- Assets/Scripts/Upgrades/UndoUpgrade.cs
+++ Assets/Scripts/Upgrades/UndoUpgrade.cs
@@ -2,15 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UndoUpgrade : MonoBehaviour {
 
+    [SerializeField] private TextMeshProUGUI inputText;
+    [SerializeField] private TextMeshProUGUI cooldownText;
+    [SerializeField] private GameObject cooldownContainer;
+
     private ShapeDropper shapeDropper;
     private Controls playerControls;
 
     private int undoCooldown = 0;
+    private float lastDropShapeTriggerTime = 0;
 
     private void Start() {
         shapeDropper = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<ShapeDropper>();
@@ -20,11 +26,29 @@ public class UndoUpgrade : MonoBehaviour {
 
     private void Update() {
         if (PauseMenu.GameIsPaused) return;
-        if (playerControls.Player.DropShape.triggered) undoCooldown--; // BUG: tijdens drop cooldown kan je dit spammen en dus de cooldown skippen
+        if (Time.time - lastDropShapeTriggerTime <= 1.0f) return; // prevents spamming undo while in dropshape cooldown
+        
+        if (playerControls.Player.DropShape.triggered && undoCooldown > 0) {
+            lastDropShapeTriggerTime = Time.time;
+            SetCooldown(undoCooldown-1);
+        }
         
         if (playerControls.Player.Undo.triggered && undoCooldown <= 0) {
-            undoCooldown = 5;
+            SetCooldown(5);
             shapeDropper.LoadTowerState();
+        }
+    }
+    
+    private void SetCooldown(int newCooldown) {
+        undoCooldown = newCooldown;
+        
+        if (undoCooldown > 0) {
+            cooldownContainer.SetActive(true);
+            cooldownText.text = undoCooldown.ToString();
+            inputText.text = "";
+        } else {
+            cooldownContainer.SetActive(false);
+            inputText.text = "Z";
         }
     }
 }
