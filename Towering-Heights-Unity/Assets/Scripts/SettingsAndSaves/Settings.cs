4676e8bc16bcfd8c7c619392640f4de682c641bc
viktorv9 <vgvermij@hotmail.com>
2026-08-01 19:17:03 +0200

    add fullscreen control, launch in windowed

--- Towering-Heights-Unity/Assets/Scripts/SettingsAndSaves/Settings.cs
+++ Towering-Heights-Unity/Assets/Scripts/SettingsAndSaves/Settings.cs
@@ -10,6 +10,7 @@ public class Settings : MonoBehaviour {
     
     [SerializeField] private Slider sfxVolumeSlider;
     [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Toggle fullscreenToggle;
     [SerializeField] private Slider cameraSensitivitySlider;
     [SerializeField] private Toggle horizontalInputInvertedToggle;
     [SerializeField] private Toggle verticalInputInvertedToggle;
@@ -17,6 +18,7 @@ public class Settings : MonoBehaviour {
     public class GameSettings {
         public float musicVolume;
         public float sfxVolume;
+        public bool fullscreen;
         public float cameraSensitivity;
         public bool invertCameraX;
         public bool invertCameraY;
@@ -31,6 +33,31 @@ public class Settings : MonoBehaviour {
     
     private void OnEnable() {
         SceneManager.sceneLoaded += Setup;
+        OnUpdateSettings += SetFullscreen;
+    }
+    
+    private void OnDisable() {
+        SceneManager.sceneLoaded -= Setup;
+        OnUpdateSettings -= SetFullscreen;
+    }
+    
+    private void SetFullscreen(GameSettings newGameSettings) {
+        Debug.Log("SetFullscreen: " + newGameSettings.fullscreen);
+        if (newGameSettings.fullscreen) {
+            Resolution current = Screen.currentResolution;
+
+            Screen.SetResolution(
+                current.width,
+                current.height,
+                FullScreenMode.FullScreenWindow
+            );
+        } else {
+            Screen.SetResolution(
+                1280,
+                720,
+                FullScreenMode.Windowed
+            );
+        }
     }
     
     private void Setup(Scene scene, LoadSceneMode mode) {
@@ -49,6 +76,12 @@ public class Settings : MonoBehaviour {
         musicVolumeSlider.onValueChanged.AddListener(delegate {
             gameSettings.musicVolume = musicVolumeSlider.value;
             PlayerPrefs.SetFloat("MusicVolume", gameSettings.musicVolume);
+            OnUpdateSettings?.Invoke(gameSettings);
+        });
+        
+        fullscreenToggle.onValueChanged.AddListener(delegate {
+            gameSettings.fullscreen = fullscreenToggle.isOn;
+            PlayerPrefs.SetInt("Fullscreen", gameSettings.fullscreen ? 1 : 0);
             OnUpdateSettings?.Invoke(gameSettings);
         });
         
@@ -73,17 +106,20 @@ public class Settings : MonoBehaviour {
     {
         gameSettings.musicVolume = PlayerPrefs.GetFloat("MusicVolume", sfxVolumeSlider.value);
         musicVolumeSlider.value = gameSettings.musicVolume;
-
         gameSettings.sfxVolume = PlayerPrefs.GetFloat("SfxVolume", musicVolumeSlider.value);
         sfxVolumeSlider.value = gameSettings.sfxVolume;
         
+        Debug.Log("PlayerPrefs.GetInt(\"Fullscreen\") " + PlayerPrefs.GetInt("Fullscreen"));
+        
+        gameSettings.fullscreen = Convert.ToBoolean(
+            PlayerPrefs.GetInt("Fullscreen", fullscreenToggle.isOn ? 1 : 0));
+        fullscreenToggle.isOn = gameSettings.fullscreen;
+        
         gameSettings.cameraSensitivity = PlayerPrefs.GetFloat("CameraSensitivity", cameraSensitivitySlider.value);
         cameraSensitivitySlider.value = gameSettings.cameraSensitivity;
-        
         gameSettings.invertCameraX = Convert.ToBoolean(
             PlayerPrefs.GetInt("HorizontalInputInverted", horizontalInputInvertedToggle.isOn ? 1 : 0));
         horizontalInputInvertedToggle.isOn = gameSettings.invertCameraX;
-        
         gameSettings.invertCameraY = Convert.ToBoolean(
             PlayerPrefs.GetInt("VerticalInputInverted", verticalInputInvertedToggle.isOn ? 1 : 0));
         verticalInputInvertedToggle.isOn = gameSettings.invertCameraY;
