6b5765e5b9e24656aee33991ba0e910ade20a9f6
viktorv9 <vgvermij@hotmail.com>
2026-08-01 17:57:22 +0200

    burst of wind on block spawn

--- Towering-Heights-Unity/Assets/Scripts/Interactables/WindArea.cs
+++ Towering-Heights-Unity/Assets/Scripts/Interactables/WindArea.cs
@@ -22,11 +22,19 @@ public class WindArea : MonoBehaviour {
         
         if (!lastEffectTime.ContainsKey(obj)) {
             lastEffectTime[obj] = Time.time - Random.Range(0.0f, 5.0f);
+
+            if (!Physics.Raycast(obj.transform.position, -transform.forward, out RaycastHit _, 4)) {
+                Instantiate(
+                    windCollisionEffect,
+                    obj.transform.position - transform.forward * Random.Range(1.0f, 1.5f),
+                    Quaternion.identity
+                );
+            }
         }
 
         Debug.DrawRay(obj.transform.position, transform.forward * -4, Color.blue);
 
-        if (!Physics.Raycast(obj.transform.position, -transform.forward, out RaycastHit hit, 4)) {
+        if (!Physics.Raycast(obj.transform.position, -transform.forward, out RaycastHit _, 4)) {
             obj.transform.parent.GetComponent<Rigidbody>()
                 .AddForce(transform.forward * windStrength);
 
